e31f1438f24b3927bd9b2a13252251f463912780
Denis Chenu <denis@sondages.pro>
2018-06-23 00:39:28 +0200

    [fix] Avoid CRSF issue after login

--- responseListAndManage.php
+++ responseListAndManage.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2018 Denis Chenu <http://www.sondages.pro>
  * @license GPL v3
- * @version 0.12.3
+ * @version 0.12.5
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
@@ -242,7 +242,7 @@ class responseListAndManage extends PluginBase {
         if (Permission::model()->hasSurveyPermission($surveyId, 'responses', 'read')) {
             if(App()->getRequest()->isPostRequest && App()->getRequest()->getPost('login_submit')) {
                 /* redirect to avoid CRSF when reload */
-                //~ Yii::app()->getController()->redirect(array("plugins/direct", 'plugin' => get_class(),'sid'=>$surveyId));
+                Yii::app()->getController()->redirect(array("plugins/direct", 'plugin' => get_class(),'sid'=>$surveyId));
             }
             $userHaveRight = true;
         }

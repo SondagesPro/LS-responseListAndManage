e3e6ecf38444239df51623bb92f1a2b1affcc4c3
Chenu Denis <denis@sondages.pro>
2018-09-07 12:37:25 +0000

    [doc] Fix version

--- responseListAndManage.php
+++ responseListAndManage.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2018 Denis Chenu <http://www.sondages.pro>
  * @license GPL v3
- * @version 1.4.3
+ * @version 1.4.5
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by

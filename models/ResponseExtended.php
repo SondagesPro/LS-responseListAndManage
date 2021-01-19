2a73ad1bb8777275f276d4fd5672b84f59a9a175
Denis Chenu <denis@sondages.pro>
2021-01-19 09:48:32 +0100

    [fix] Better voiew for date dropdown

--- models/ResponseExtended.php
+++ models/ResponseExtended.php
@@ -551,10 +551,10 @@ class ResponseExtended extends LSActiveRecord
         $attributes = $this->attributes;
         $minValue = !empty($attributes[$column]['min']) ? $attributes[$column]['min'] : null;
         $maxValue = !empty($attributes[$column]['max']) ? $attributes[$column]['max'] : null;
-        $dateFilter = '<div class="input-group input-group-date input-group-date-min"><div class="input-group-addon">&gt;=</div>';
+        $dateFilter = '<div class="input-group input-group-date input-group-date-min"><div class="input-group-addon input-sm">&gt;=</div>';
         $dateFilter.= CHtml::textField(get_class($this)."[".$column."]"."[min]",$minValue,array("class"=>'form-control input-sm filter-date','data-format'=>$dateFormatMoment));
         $dateFilter.= "</div>";
-        $dateFilter .= '<div class="input-group input-group-date input-group-date-max"><div class="input-group-addon">&lt;</div>';
+        $dateFilter .= '<div class="input-group input-group-date input-group-date-max"><div class="input-group-addon input-sm">&lt;</div>';
         $dateFilter.= CHtml::textField(get_class($this)."[".$column."]"."[max]",$maxValue,array("class"=>'form-control input-sm filter-date','data-format'=>$dateFormatMoment));
         $dateFilter.= "</div>";
         $dateFilter.= CHtml::hiddenField(get_class($this)."[".$column."]"."[format]",$dateFormatPHP);
